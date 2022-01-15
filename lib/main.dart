4eb35be94f15e138068f82697a65b3237cc101b2
mjkt39 <k.miyaji@swallowarc.jp>
2022-01-15 22:42:00 +0900

    delete firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:porker_front/routes/routes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  FirebaseAnalytics analytics = FirebaseAnalytics();
 
   runApp(
     ProviderScope(
@@ -23,9 +19,6 @@ void main() {
         ),
         initialRoute: '/',
         onGenerateRoute: RouteConfiguration.onGenerateRoute,
-        navigatorObservers: [
-          FirebaseAnalyticsObserver(analytics: analytics),
-        ],
       ),
     ),
   );
